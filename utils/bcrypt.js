c7c84a6998868c5524823eda8531314b372d1f71
JulietYew <juliecj24@gmail.com>
2023-05-24 18:48:05 +0100

    feat: added the verify and hashpassword code

---
+++ utils/bcrypt.js
@@ -0,0 +1,18 @@
+const bcryptjs = require('bcryptjs')
+const rounds = (process.env.ROUNDS )
+
+
+// Masks the passsword with random characters to protect user data
+const hashPassword = async (password) => {
+    const salt = await bcryptjs.genSalt(rounds)
+    password = await bcryptjs.hash(password, salt)
+    return password
+}
+
+// Confirms the input password is the same password that was masked when the user signed up
+const verifyPassword = async (adminPassword, hashedAdminPassword) => {
+    const isValid = await bcryptjs.compare(adminPassword, hashedAdminPassword)
+    return isValid
+}
+
+export { hashPassword, verifyPassword }
