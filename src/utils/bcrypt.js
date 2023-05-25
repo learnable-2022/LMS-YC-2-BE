feed294541593c77ee428b767b19afbdd955e901
Gift <Giftibe62@gmail.com>
2023-05-25 16:47:29 +0100

    fix: bug in admin login and signup

--- src/utils/bcrypt.js
+++ src/utils/bcrypt.js
@@ -1,22 +1,29 @@
-const bcrypt = require('bcryptjs')
-const rounds = (process.env.ROUNDS )
+const bcrypt = require('bcrypt')
+const rounds = parseInt(process.env.ROUNDS)
 
 
-// Masks the passsword with random characters to protect user data
-const hashPassword = async (password) => {
-    const salt = await bcrypt.genSaltSync(rounds)
-    const pass = await bcrypt.hashSync(password, salt)
-    return pass
-}
+
+
 
 // Confirms the input password is the same password that was masked when the user signed up
 const verifyPassword = async (adminPassword, hashedAdminPassword) => {
-    const isValid = await bcrypt.compareSync(adminPassword, hashedAdminPassword)
-    return isValid
-}
-const verifyUserPassword = async (userPassword, hashedUserPassword) => {
-    const isValid = await bcrypt.compareSync(userPassword, hashedUserPassword)
-    return isValid
+    const check = await bcrypt.compare(userPassword, hashedAdminPassword)
+    if (match) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
-module.exports = { hashPassword, verifyPassword, verifyUserPassword }
+const verifyUserPassword = async (userPassword, hashedUserPassword) => {
+    const check = await bcrypt.compare(userPassword, hashedUserPassword)
+    if (match) {
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+module.exports = { verifyPassword, verifyUserPassword }
