6e073c49306594f6c4ff9ebc91c6d2f817d525a0
Gift <Giftibe62@gmail.com>
2023-06-09 11:12:18 +0100

    fix: verification

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken')
+const { MESSAGES } = require('../config/constant.config')
 const userModel = require('../model/user.model')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
@@ -7,52 +8,14 @@ dotenv.config();
 // a middleware to autheticate users 
 
 const auth = async (req, res, next) => {
-    try {
-        console.log('first one')
-        let token = ''
-        //Gets token from client header (like a cookie stored or attached to a user's unique id from the client side after they sign up or sign in)
-        if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-            token = req.headers.authorization.split(' ')[1];
-        } else {
-            // This is an alternate method of getting it from previously set cookies for the purpose of testing
-            token = req.cookies.token
-        }
-        if (!token) {
-            return res.status(401).send({
-                message: 'Please sign in',
-                success: false
-            })
-        }
-        console.log('second one')
-        const verified = jwt.verify(token, process.env.SECRET_KEY)
-        if (!verified) {
-            return res.status(401).send({
-                message: 'User Verification failed',
-                success: false
-            })
-        }
-        const user = await userModel.findOne({
-            _id: verified._id,
-            'tokens.token': token
-        })
-        if (!user) {
-            return res.status(401).send({
-                message: 'You are not verified',
-                success: false
-            })
-        }
-        console.log('User Authentication successful')
-
-        req.token = token
-        req.user = user
+    const bearHeader = req.headers['authorization']
+    if (typeof bearHeader !== 'undefined') {
+        const bearToken = bearHeader.split(' ')[1]
+        req.token = bearToken
         next()
-
-    } catch (error) {
-        return res.status(500).send({
-            message: 'Internal Server Error' + err,
-            success: false
-        })
-
+    } else {
+        res.status(403)
+            .send({ message: err.message || MESSAGES.USER.UNAUTHORIZED, success: false }); // Restricting access if authorization fails 
     }
 }
 module.exports = auth
